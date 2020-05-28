9ded7fb3dd86241a7cf0ffe10265d63ff161edd9
paulinellyoliveira <paulinellyoliveira@hotmail.com>
2020-05-27 23:54:34 -0300

    Add function to item delete in /appweb.

--- appweb/src/pages/product/index.js
+++ appweb/src/pages/product/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import api from "../../services/api"
+import api from "../../services/api";
+import { Redirect } from "react-router-dom";
 
 import "./styles.css";
 
 export default class Product extends Component{
+    
     state = {        
-        product: {},        
-    };
+        product: {}, redirect: false,        
+    };    
     
     async componentDidMount(){
         const { id } = this.props.match.params;
@@ -16,18 +18,40 @@ export default class Product extends Component{
         this.setState({ product: response.data });
     }
 
+    async excluir (id) {               
+        console.log("Processando: ");
+
+        const response = await api.delete(`/products/${id}`);
+
+        console.log("Excluido - id: " + id);                
+
+        this.setState({ redirect: true });
+    }       
+
     render () {
-        const { product } = this.state;
-
+        const { product } = this.state; 
+                
+        if (this.state.redirect) {            
+            return <Redirect to={"/"} />
+        }          
+        
         return (
-            <div className="product-info">
-                <h1>{ product.title }</h1>
+            <div>
+                <div className="product-info">
+                    <h1>{ product.title }</h1>
 
-                <p>{ product.description }</p>
+                    <p>{ product.description }</p>
 
-                <p>
-                    URL: <a href={ product.url }>{ product.url }</a>
-                </p>
+                    <p>
+                        URL: <a href={ product.url }>{ product.url }</a>
+                    </p>
+                </div>
+
+                <div className="product-action">
+
+                    <button onClick= { () => this.excluir(product._id) } >Excluir</button>                                                                                             
+                                                                                     
+                </div>
             </div>
         );
     }
